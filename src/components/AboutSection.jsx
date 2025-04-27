7aa6e87be63233ab417c842f0e33625007afbbcd
Richard Oates <richoates2020@gmail.com>
2025-04-27 19:55:13 +0200

    Menu button styling

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -8,8 +8,8 @@ export default function AboutSection() {
                 <div data-aos="fade-zoom-in" data-aos-anchor-placement="center-bottom" className="profile-image-container">
                     <img src={jess} className="profile-image" />
                 </div>
-                <p data-aos="fade-right" >My name's Jess, I'm the makeup artist behind Queen Bee Beauty, based in Sheffield.</p>
-                <p data-aos="fade-right" >I specialise in all types of glam makeup e.g soft glam, full glam, dewy and nude looks.</p>
+                <p data-aos="fade-right" >My name is Jess, I'm the makeup artist behind QB Beauty, based in Sheffield.</p>
+                <p data-aos="fade-right" >I'm passionate about all types of glam makeup e.g soft glam, full glam, dewy and nude looks.</p>
                 <p data-aos="fade-right" >Prices are £15-£20 (cash only).</p>
                 <p data-aos="fade-right" >Appointments available on Saturdays.</p>
                 <p data-aos="fade-right" >Please leave me a dm or message me to book and discuss your needs.</p>
