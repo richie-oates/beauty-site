28bf0422e88709fb794225f9f459f0f9a62359f1
Richard Oates <richoates2020@gmail.com>
2025-04-27 09:35:49 +0200

    Responsiveness on main image and font sizes

--- src/components/HomeSection.jsx
+++ src/components/HomeSection.jsx
@@ -1,13 +1,21 @@
 import logo from '../assets/qb_logo.jpg'
 import example_1 from '../assets/qb_example_01.jpg'
 import example_2 from '../assets/qb_example_02.jpg'
-import makeup from '../assets/makeup_02.jpg'
+import makeup_s from '../assets/makeup_02_small.jpg'
+import makeup_m from '../assets/makeup_02_med.jpg'
+import makeup_l from '../assets/makeup_02_large.jpg'
+import { useMediaQuery } from 'react-responsive';
+
+function select_image() {
+    if (useMediaQuery({ query: '(max-width: 600px' })) return makeup_s
+    else return makeup_l
+}
 
 export default function HomeSection() {
     return (
         <section id="home" >
             <div className="impact-image-container">
-                <img className="impact-image" src={makeup} />
+                <img className="impact-image" src={select_image()} />
             </div>
             <div >
                 <img className="main-logo" src={logo}></img>
