628274a58172a6f03c1e8cfca76b93815b6e11c0
Richard Oates <richoates2020@gmail.com>
2025-05-28 16:55:27 +0200

    contact form setup

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -13,10 +13,10 @@ export default function AboutSection() {
                     <div className='text-content'>
                         <a href='#about'><h1 data-aos="fade-right" data-aos-anchor-placement="bottom-bottom" >About Me</h1></a>
                         <p data-aos="fade-right" >My name is <b>Jess</b>, I'm the makeup artist behind <b>QB Beauty</b>, based in Sheffield.</p>
-                        <p data-aos="fade-right" >I offer all types of <b>glam</b> makeup e.g <b>soft glam</b>, <b>full glam</b>, <b>dewy</b> and <b>nude</b> looks.</p>
-                        <p data-aos="fade-right" >Prices are £15-£20 (cash only).</p>
-                        <p data-aos="fade-right" >Appointments available on <b>Saturdays</b>.</p>
-                        <p data-aos="fade-right" ><a href='#contact'>Contact</a> me to book and discuss your needs.</p>
+                        <p data-aos="fade-right" >I offer all types of <b>glam</b> makeup e.g <b>soft glam</b>, <b>full glam</b>, <b>dewy</b> and <b>nude</b> looks. As well as classic lashes <a href='#services'>services</a>.</p>
+                        {/* TODO: Include this line after setting up booking functionality */}
+                        {/* <p data-aos="fade-right" ><a href='#services'>Appointments</a> can be requested through the booking form</p> */}
+                        <p data-aos="fade-right" ><a href='#contact'>Contact</a> me directly for general enquiries.</p>
                     </div>
                 </div>
             </div>
--- src/components/ContactSection.jsx
+++ src/components/ContactSection.jsx
@@ -12,13 +12,15 @@ export default function ContactSection() {
                     <SocialIcon className='social-icon' url='https://www.facebook.com/jessica.broxholme.50' />
                     <SocialIcon className='social-icon' url='https://www.instagram.com/qb.beauty.official/' />
                 </div>
-                <p >Get in touch for bookings or enquiries.</p>
+                <p>Get in touch to discuss your beauty needs.</p>
                 <p>Email: <a href="mailto:qb.beauty.official@gmail.com">qb.beauty.official@gmail.com</a></p>
                 <p>or use this form:</p>
-                <form className='contact-form' >
-                    <input type="text" placeholder="Your Name" />
-                    <input type="email" placeholder="Your Email" />
-                    <textarea placeholder="Your Message"></textarea>
+                <form name="contact" method="POST" data-netlify="true" netlify-honeypot="bot-field" className='contact-form' >
+                    <input type="hidden" name="form-name" value="contact" />
+                    <input type="text" name="bot-field" hidden />
+                    <input type="text" placeholder="Your Name" required />
+                    <input type="email" placeholder="Your Email" required />
+                    <textarea placeholder="Your Message" required></textarea>
                     <button type="submit">Send Message</button>
                 </form>
 
